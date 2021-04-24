703219ecab9e01cca2d041a72921eedf03d27d5a
Jessica <simply.me05@hotmail.com>
2021-04-24 17:15:35 -0400

    setting up files for routes and models

---
+++ Develop/models/index.js
@@ -0,0 +1,42 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  through: {
+    model: ProductTag,
+  },
+  // Define an alias for when data is retrieved
+  as: 'products_tagged'
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+  through: {
+    model: ProductTag,
+  },
+   // Define an alias for when data is retrieved
+   as: 'tagged_products'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
